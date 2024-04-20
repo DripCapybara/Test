ba6c3fb866140155578a7325f51e1676f6793456
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-04-20 01:08:18 -0400

    SCP096 CS

---
+++ SCP-096-Client-Side.lua
@@ -0,0 +1,51 @@
+-- SCP - 096
+-- Not FE
+-- Made By DripCapybara
+
+-- Credits to:
+
+--Brutez model & scripting
+
+local SCP = game:GetObjects("rbxassetid://14186793613")[1]
+SCP["SCP-096"].Parent = workspace
+SCP = workspace["SCP-096"]
+SCP:PivotTo(game.Players.LocalPlayer.Character:GetPivot()*CFrame.new(0,0,-20))
+
+local function Load(Obj, Url)
+local function GiveOwnGlobals(Func, Script)
+    local Fenv = {}
+    local RealFenv = {script = Script}
+    local FenvMt = {}
+    FenvMt.__index = function(a,b)
+        if RealFenv[b] == nil then
+            return getfenv()[b]
+        else
+            return RealFenv[b]
+        end
+    end
+    FenvMt.__newindex = function(a, b, c)
+        if RealFenv[b] == nil then
+            getfenv()[b] = c
+        else
+            RealFenv[b] = c
+        end
+    end
+    setmetatable(Fenv, FenvMt)
+    setfenv(Func, Fenv)
+    return Func
+end
+
+local function LoadScripts(Script)
+    if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
+        spawn(function()
+            GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script)()
+        end)
+    end
+    for i,v in pairs(Script:GetChildren()) do
+        LoadScripts(v)
+    end
+end
+
+LoadScripts(Obj)
+end
+Load(SCP)
