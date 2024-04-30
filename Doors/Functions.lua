2a17d2cb5173a01380c88ca5bcc8ab413ff7ff53
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-04-30 00:35:13 -0400

    Functions for whatever thing i make

---
+++ Doors/Functions.lua
@@ -0,0 +1,85 @@
+--// Made By DripCapybara
+
+local module = {}
+
+local Plr = game.Players.LocalPlayer
+
+local ModuleScripts = {
+	MainGame = require(Plr.PlayerGui.MainUI.Initiator.Main_Game),
+	Events = require(game:GetService("ReplicatedStorage").ClientModules.Module_Events),
+}
+
+--// Area function
+function module.AreaLocation(Area)
+  ModuleScripts.MainGame.titlelocation(Area)
+end
+
+--// Caption support for non-supporting firesignal executors
+function module.Caption(Msg,Dur)
+       ModuleScripts.MainGame.caption(Msg,Dur,true)
+end
+
+--// Spawns Glitch
+function module.InitGlitch()
+      require(game:GetService("ReplicatedStorage").ClientModules.EntityModules.Glitch).stuff(ModuleScripts.MainGame)
+end
+
+--// Light Flicker for non-supporting firesignal executors
+function module.flickerLights(room, dur)
+       ModuleScripts.Events.flicker(room,dur)
+end
+
+--// Get Git(hub) Image
+function module.GetGitImage(Path,AssetName)
+	local url=Path
+	
+	if not isfile(AssetName..".png") then
+		writefile(AssetName..".png", game:HttpGet(url))
+	end
+	
+	return (getcustomasset or getsynasset)(AssetName..".png")
+end
+
+--// Get Git(hub) Sound
+function module.GetGitSound(Path,AssetName)
+    local url=Path
+
+    if not isfile(AssetName..".mp3") then 
+        writefile(AssetName..".mp3", game:HttpGet(url)) 
+    end
+
+    local sound=Instance.new("Sound")
+    sound.SoundId=(getcustomasset or getsynasset)(AssetName..".mp3")
+    return sound 
+end
+
+--// Get Git(hub) Asset
+function module.GetAsset(Path,AssetName)
+       --// Get Type by matching if its a mp3 or not.
+       local Type = string.match(Path,".mp3") and "Sound" or "Image"
+       
+       --// If is a sound.
+       if Type == "Sound" then
+               return GetGitSound(Path,AssetName)
+       end
+       --// If is a Image
+       if Type == "Image" then
+               return GetGitImage(Path,AssetName)
+       end
+       --// If type wasnt detected
+       if Type == nil then
+               --// Is a roblox asset
+               if string.match(Path,"rbxassetid://") then
+                       return game:GetObjects(Path)[1]
+               end
+       end
+end
+
+-- Main
+for name, func in module do
+    if typeof(func) == "function" then
+        getgenv()[name] = func
+    end
+end
+
+return module
