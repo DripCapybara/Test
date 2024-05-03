6466f92c1e1eaad097445374e5d8418ae5567be1
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-05-03 13:41:41 -0400

    Flashlight CS

---
+++ FlashLight-Client-Side.lua
@@ -0,0 +1,4 @@
+local fl = game:GetObjects("rbxassetid://11242492137")[1]
+fl.Parent = game.Players.LocalPlayer.Backpack
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Test/main/ScriptLoader.lua"))()(fl)
