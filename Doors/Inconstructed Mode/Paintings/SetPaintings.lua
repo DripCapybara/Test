ba1f436de247f0d2356d9464234e77f1515df6d4
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-05-10 10:27:46 -0400

    Set Paintings

---
+++ Doors/Inconstructed Mode/Paintings/SetPaintings.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {"rbxassetid://11881132074","Despair"},
+	[2] = {"rbxassetid://11881132745","Odd feline Specimen"},
+	[3] = {"rbxassetid://11881654771","A tryhard..."},
+	[4] = {"rbxassetid://7084794697","him."},
+	[5] = {"https://i.kym-cdn.com/photos/images/newsfeed/001/996/641/bc2.jpg","Lolololo"},
+	[6] = {"https://github.com/MuhXd/DoorSuff/blob/main/DoorsModes/Png.png?raw=true","Depth"},
+	[7] = {"https://is3-ssl.mzstatic.com/image/thumb/Purple125/v4/00/f3/0a/00f30a77-8163-8757-a4e4-6312063b8de9/source/512x512bb.jpg",'ROUX'},
+	[8] = {"https://github.com/MuhXd/Images/blob/main/white_1_painting.png?raw=true","???"},
+	[9] = {"https://github.com/MuhXd/Images/blob/main/maxwell_the_cat.jpeg?raw=true","Well cat"},
+	[10] = {"https://github.com/MuhXd/Images/blob/main/SPOILER_20220527_094645.jpg?raw=true","Bathroom"}
+}
