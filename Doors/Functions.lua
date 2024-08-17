b1ed67715c99b6c413ce5d9a9b21ebcb7956e66a
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-08-17 13:26:22 -0400

    Fix

--- Doors/Functions.lua
+++ Doors/Functions.lua
@@ -5,42 +5,42 @@ local module = {}
 local Plr = game.Players.LocalPlayer
 
 local ModuleScripts = {
-	MainGame = require(Plr.PlayerGui.MainUI.Initiator.Main_Game),
-	Events = require(game:GetService("ReplicatedStorage").ClientModules.Module_Events),
+   MainGame = require(Plr.PlayerGui.MainUI.Initiator.Main_Game),
+   Events = require(game:GetService("ReplicatedStorage").ClientModules.Module_Events),
 }
 
 --// Area function
 function module.AreaLocation(Area)
-  ModuleScripts.MainGame.titlelocation(Area)
+   ModuleScripts.MainGame.titlelocation(Area)
 end
 
 --// Caption support for non-supporting firesignal executors
 function module.Caption(Msg,Dur)
-       ModuleScripts.MainGame.caption(Msg,Dur,true)
+   ModuleScripts.MainGame.caption(Msg,true,Dur)
 end
 
 --// Spawns Glitch
 function module.InitGlitch()
-      require(game:GetService("ReplicatedStorage").ClientModules.EntityModules.Glitch).stuff(ModuleScripts.MainGame)
+   require(game:GetService("ReplicatedStorage").ClientModules.EntityModules.Glitch).stuff(ModuleScripts.MainGame)
 end
 
 --// Light Flicker for non-supporting firesignal executors
 function module.flickerLights(room, dur)
-       ModuleScripts.Events.flicker(room,dur)
+   ModuleScripts.Events.flicker(room,dur)
 end
 
---// Get Git(hub) Image
+--// Get Git Image
 function module.GetGitImage(Path,AssetName)
-	local url=Path
+   local url=Path
 	
-	if not isfile(AssetName..".png") then
-		writefile(AssetName..".png", game:HttpGet(url))
-	end
+   if not isfile(AssetName..".png") then
+      writefile(AssetName..".png", game:HttpGet(url))
+   end
 	
-	return (getcustomasset or getsynasset)(AssetName..".png")
+   return (getcustomasset or getsynasset)(AssetName..".png")
 end
 
---// Get Git(hub) Sound
+--// Get Git Sound
 function module.GetGitSound(Path,AssetName)
     local url=Path
 
@@ -53,26 +53,26 @@ function module.GetGitSound(Path,AssetName)
     return sound 
 end
 
---// Get Git(hub) Asset
+--// Get Git Asset
 function module.GetAsset(Path,AssetName)
-       --// Get Type by matching if its a mp3 or not.
-       local Type = string.match(Path,".mp3") and "Sound" or "Image"
+    --// Get Type by matching if its a mp3 or not.
+    local Type = string.match(Path,".mp3") and "Sound" or "Image"
        
-       --// If is a sound.
-       if Type == "Sound" then
-               return GetGitSound(Path,AssetName)
-       end
-       --// If is a Image
-       if Type == "Image" then
-               return GetGitImage(Path,AssetName)
-       end
-       --// If type wasnt detected
-       if Type == nil then
-               --// Is a roblox asset
-               if string.match(Path,"rbxassetid://") then
-                       return game:GetObjects(Path)[1]
-               end
-       end
+    --// If type is a sound.
+    if Type == "Sound" then
+       return GetGitSound(Path,AssetName)
+    end
+    --// If type is a Image
+    if Type == "Image" then
+        return GetGitImage(Path,AssetName)
+    end
+    --// If type wasnt detected
+    if Type == nil then
+       --// If type is a roblox asset
+        if string.match(Path,"rbxassetid://") then
+           return game:GetObjects(Path)[1]
+        end
+    end
 end
 
 -- Main
