5b762d2b579b571f8723b9ddc9d15e45a7890fae
DripCapybara <167495373+DripCapybara@users.noreply.github.com>
2024-12-27 18:31:48 -0300

    Delete FlashLight-Client-Side.lua

--- FlashLight-Client-Side.lua
+++
@@ -1,4 +0,0 @@
-local fl = game:GetObjects("rbxassetid://11242492137")[1]
-fl.Parent = game.Players.LocalPlayer.Backpack
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Test/main/ScriptLoader.lua"))()(fl)
